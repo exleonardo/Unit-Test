7d4097f49e5a6de88f45e746a86af7cb31d123ff
Exleonardo <exleonardo@mail.ru>
2023-07-17 13:26:58 +0300

    add new test

--- src/unit test/09/09_01.test.ts
+++ src/unit test/09/09_01.test.ts
@@ -19,4 +19,61 @@ test("big test",()=>{
 
   expect(user.age).toBe(33)
 
+  const superman = user
+  superman.age  = 1000
+  expect(user.age).toBe(1000)
+
+})
+
+test("big test",()=>{
+
+  const users=[{
+    name:"Dimych",
+    age:32,
+  },{
+    name:"Dimych",
+    age:32,
+  }];
+  const admins = users
+  admins.push({name:"Bandygan",age: 10})
+
+
+  expect(users[2]).toEqual({name:"Bandygan",age: 10})
+
+
+})
+
+
+test("value type test",()=>{
+
+let userCount  = 100;
+let adminsCount = userCount;
+
+adminsCount = adminsCount+1;
+  adminsCount++
+
+  expect(adminsCount).toBe(102)
+
+
+})
+
+test("big test",()=>{
+
+  const user={
+    name:"Dimych",
+    age:32,
+    adress:{title:"Minsk"}
+  }
+  icreaseAge(user)
+
+let addAdr = user.adress
+
+  let user2 = {
+    name:"Dimych",
+    age:32,
+    adress:addAdr
+  }
+
+  expect(user.age).toBe(1000)
+
 })
